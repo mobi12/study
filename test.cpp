d4dc1ee01e33c2bae5bd05b41b16edcc430d3835
mobi12 <mc1a1@hotmail.com>
2015-03-31 20:58:46 +0800

    C++ primer plus的试题，于第二章末尾

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+//一个试题，出自C++ primer plus
+//同时也为了测试github
+using namespace std;
+
+int main()
+{
+	cout <<"Alan.Zhang" << endl; //我也入乡随俗的写了个洋文名字
+	cout <<"now,I'm going Cambrige" << endl;
+
+	return 0;
+}
