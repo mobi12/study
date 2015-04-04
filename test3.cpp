d5e185f352769e2959a1a184bb713e9e46e935c7
mobi12 <mc1a1@hotmail.com>
2015-04-04 17:18:19 +0800

    have a bug

---
+++ test3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x;
+	int num[x];
+	cout << "输入一个临界值" << endl;
+	cin >> x;
+	cout << "输入一些数字吧" << endl;
+	for (int z=0;z<6;z++)
+	{
+		cin >> num[x];
+	}
+
+	return 0;
+}
