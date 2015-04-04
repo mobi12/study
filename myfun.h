64e05d77409ec528b80a8b0e625890f4f9f7a702
mobi12 <mc1a1@hotmail.com>
2015-04-04 18:06:12 +0800

    test3存在bug

--- myfun.h
+++ myfun.h
@@ -23,4 +23,4 @@ namespace my
 		}
 	return temp;
 	}
-}	
+}
--- test3.cpp
+++ test3.cpp
@@ -9,9 +9,14 @@ int main()
 	cout << "输入一个临界值" << endl;
 	cin >> x;
 	cout << "输入一些数字吧" << endl;
-	for (int z=0;z<6;z++)
+	for (int z=0;z<x;z++)
 	{
 		cin >> num[x];
+	}
+	cout << "输出数字" << endl;
+	for (int y=0;y<x;y++)
+	{
+		cout << num[x] << endl;
 	}
 
 	return 0;
