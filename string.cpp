3e53c5487b7b0ae20ca5ddf109d3aedb3408ad5e
mobi12 <mc1a1@hotmail.com>
2015-04-09 10:10:55 +0800

    用sizeof操作符测试了一下string类的长度

--- string.cpp
+++ string.cpp
@@ -6,6 +6,11 @@ using namespace std;
 int main()
 {
 	string fetch="错误！其实没错(^_^)";
+	char charry[18]="风雨里追赶";
 	cout << fetch << endl;
+	cout << charry << endl;
+	cout << sizeof fetch << endl;
+	cout << sizeof charry << endl;
+
 	return 0;
 }
