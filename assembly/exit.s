a670b3f84d0fe3d350285892931aac11fe31a5eb
mobi12 <mc1a1@hotmail.com>
2015-05-13 09:37:56 +0800

    加入注释

--- assembly/exit.s
+++ assembly/exit.s
@@ -1,8 +1,9 @@
+#退出程序并返回状态值5
 .section .data
 .section .text
 .globl _start
 _start:
-movl $1, %eax
-movl $5, %ebx
+movl $1, %eax #把1以立即寻址方式放到寄存器eax，1代表exit
+movl $5, %ebx #返回值5
 
-int $0x80
+int $0x80 #调用系统中断
