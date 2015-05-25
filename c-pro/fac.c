0e733b346935d8d2ff5a0c1c09d00e99525c4a02
mobi12 <mc1a1@hotmail.com>
2015-05-25 13:17:00 +0800

    阶乘c语言版

---
+++ c-pro/fac.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+//阶乘
+int main()
+{
+	int a = 1;
+	int b = 2;
+	int c = 0;
+	int d = 0;
+
+	scanf("%d",&c);
+	for(;c>d;d++)
+	{
+		a=a*b;
+		b++;
+		printf("%d\n",a);
+	}
+
+	return 0;
+}
+		
