c81b81e85708db7ae2ea19d6ff2ec5063c7fcec9
mobi12 <mc1a1@hotmail.com>
2015-06-29 14:11:27 +0800

    练习题4.11第4题

---
+++ cpp/cpp4114.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string fname;
+	string lname;
+
+	cout << "输入你的姓氏" << endl;
+	cin >> fname;
+	cout << "输入你的名字" << endl;
+	cin >> lname;
+
+	cout << fname << ", " << lname << endl;
+
+	return 0;
+}
