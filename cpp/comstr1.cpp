55045a51341a1d62e2a11c882a2b69ff089e433c
mobi12 <mc1a1@hotmail.com>
2015-07-04 15:28:12 +0800

    程序清单5.11

---
+++ cpp/comstr1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+	char word[5] = "?ate";
+
+	for (char ch = 'a'; strcmp(word,"mate"); ch++)
+	{
+		cout << word << endl;
+		word[0] = ch;
+	}
+
+	cout << "After loop ends, word is " << word << endl;
+
+	return 0;
+}
