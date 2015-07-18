41a6add30f0bdf8d46138ab57aee87d075a0a2ed
mobi12 <mc1a1@hotmail.com>
2015-07-18 15:59:42 +0800

    犯了个低级错误

--- cpp/myfun.h
+++ cpp/myfun.h
@@ -1,3 +1,5 @@
+#include <iostream>
+
 namespace my
 {
 	//比较大小函数
@@ -28,7 +30,7 @@ namespace my
 	//菜单函数
 	void menu(void)
 	{
-		cout << "这是一个菜单\n"
+		std::cout << "这是一个菜单\n"
 			<< "有以下选项：" << "1)呵呵：无聊的作者无聊的功能\n"
 			<< "2)我去：将我去写入文件\n"
 			<< "3)我都不知道该干嘛，退了算了\n";
