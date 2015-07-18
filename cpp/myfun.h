f4e80b85181494bc1d8c845cf7aef71ee5ae2267
mobi12 <mc1a1@hotmail.com>
2015-07-18 15:17:00 +0800

    加了一个没什么卵用的函数

--- cpp/myfun.h
+++ cpp/myfun.h
@@ -1,5 +1,6 @@
 namespace my
 {
+	//比较大小函数
 	int getMax(int arr[0],int count,bool istrue)
 	{
 		int temp = arr[0];
@@ -22,5 +23,14 @@ namespace my
 			}
 		}
 	return temp;
+	}
+
+	//菜单函数
+	void menu(void)
+	{
+		cout << "这是一个菜单\n"
+			<< "有以下选项：" << "1)呵呵：无聊的作者无聊的功能\n"
+			<< "2)我去：将我去写入文件\n"
+			<< "3)我都不知道该干嘛，退了算了\n";
 	}
 }
