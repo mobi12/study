724195face8dd1423013510473049c6adb7400f2
mobi12 <mc1a1@hotmail.com>
2015-07-25 18:43:54 +0800

    程序清单7.5--改写程序清单7.4

---
+++ cpp/arrfun2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+const int ArSize = 8;
+
+int sum_arr(int arr[], int n);
+
+int main()
+{
+	int cookies[ArSize] = {1,2,4,6,8,16,32,64};
+
+	std::cout << cookies << " = array address, ";
+
+	std::cout << sizeof cookies << " = sizeof cookies\n";
+	int sum = sum_arr(cookies, ArSize);
+	std::cout << "Total cookies eaten: " << sum << std::endl;
+	sum = sum_arr(cookies, 3);
+	
+	std::cout << "First three eaters ate " << sum << "cookies.\n";
+	sum = sum_arr(cookies + 4, 4);
+	std::cout << "Last four eaters ate " << sum << "cookies.\n";
+
+	return 0;
+}
+
+int sum_arr(int arr[], int n)
+{
+	int total = 0;
+	std::cout << arr << " = arr, ";
+
+	std::cout << sizeof arr << " = sizeof arr\n";
+	for (int i = 0; i < n; i++)
+		total = total + arr[i];
+	return total;
+}
