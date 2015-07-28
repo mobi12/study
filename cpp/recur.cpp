74a5d7d30d26bb59be29ff658fae1277cfe19817
mobi12 <mc1a1@hotmail.com>
2015-07-28 17:43:20 +0800

    程序清单7.15--递归

---
+++ cpp/recur.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void countdown(int n);
+
+int main()
+{
+	countdown(4);
+
+	return 0;
+}
+
+void countdown(int n)
+{
+	cout << "Counting down ... " << n << endl;
+	
+	if (n > 0)
+		countdown(n - 1);
+	cout << n << ": Kaboom!" << endl;
+}
