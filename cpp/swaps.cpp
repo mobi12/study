57cfc2252f6f078a51e738608b30ede215b47426
mobi12 <mc1a1@hotmail.com>
2015-08-02 14:49:27 +0800

    程序清单8.4--函数修改参数值

---
+++ cpp/swaps.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+void swapr(int & a, int & b);
+void swapp(int * p, int *q);
+void swapv(int a, int b);
+
+int main()
+{
+	int wallet1 = 300;
+	int wallet2 = 350;
+
+	cout << "wallet1 = $" << wallet1;
+	cout << "wallet2 = $" << wallet2 << endl;
+
+	cout << "using references to swap contents: \n";
+	swapr(wallet1, wallet2);
+	cout << "wallet1 = $" << wallet1;
+	cout << "wallet2 = $" << wallet2 << endl;
+
+	cout << "using pointers to swap contents: \n";
+	swapp(&wallet1, &wallet2);
+	cout << "wallet1 = $" << wallet1;
+	cout << "wallet2 = $" << wallet2 << endl;
+
+	cout << "Trying to use passing by value: \n";
+	swapv(wallet1, wallet2);
+	cout << "wallet1 = $" << wallet1;
+	cout << "wallet2 = $" << wallet2 << endl;
+
+	return 0;
+}
+
+void swapr(int & a, int & b)
+{
+	int temp;
+
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+void swapp(int *p, int *q)
+{
+	int temp;
+
+	temp = *p;
+	*p = *q;
+	*q = temp;
+}
+
+void swapv(int a, int b)
+{
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
