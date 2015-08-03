fb16278c6bbf60fa9882e3525516a5239bb96d9a
mobi12 <mc1a1@hotmail.com>
2015-08-03 15:15:38 +0800

    程序清单8.7--第三个函数是错的，不能返回引用不能返回临时变量

---
+++ cpp/strquote.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string version1(const string & s1, const string & s2);
+const string & version2(string & s1, const string & s2);
+const string & version3(string & s1, const string & s2);
+
+int main()
+{
+	string input;
+	string copy;
+	string result;
+
+	cout << "Enter a string: ";
+	getline(cin, input);
+	copy = input;
+	cout << "Your string as entered: " << input << endl;
+	result = version1(input, "***");
+	cout << "Your string enhanced: " << result << endl;
+	cout << "Your original string: " << input << endl;
+
+	result = version2(input, "###");
+	cout << "Your string enhanced: " << result << endl;
+	cout << "Your original string: " << input << endl;
+	cout << "Resetting original string." << endl;
+	input = copy;
+	result= version3(input, "@@@");
+	cout << "Your string enhanced: " << result << endl;
+	cout << "Your original string: " << input << endl;
+
+	return 0;
+}
+
+string version1(const string & s1, const string & s2)
+{
+	string temp;
+
+	temp = s2 + s1 + s2;
+	return temp;
+}
+
+const string & version2(string & s1, const string & s2)
+{
+	s1 = s2 + s1 + s2;
+
+	return s1;
+}
+//出错的函数
+const string & version3(string  & s1, const string & s2)
+{
+	string temp;
+
+	temp = s2 + s1 + s2;
+
+	return tempu;//出错的地方
+}
