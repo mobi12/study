31ec334eca706a4b26afb4105ed693a620a56afd
mobi12 <mc1a1@hotmail.com>
2015-10-31 14:51:49 +0800

    头文件

---
+++ cpp/coordin.h
@@ -0,0 +1,19 @@
+#ifndef COORDIN_H_
+#define COORDIN_H_
+
+struct polar
+{
+	double distance;
+	double angle;
+};
+
+struct rect
+{
+	double x;
+	double y;
+};
+
+polar rect_to_polar(rect xypos);
+void show_polar(polar dapos);
+
+#endif
