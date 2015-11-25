6ba6096e420e264df8b8796bbbbf6ba840c16adf
mobi12 <mc1a1@hotmail.com>
2015-11-25 22:13:27 +0800

    友元函数--实现操作符重载

---
+++ cpp/usemytime3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "mytime3.h"
+
+int main()
+{
+    using std::cout;
+    using std::endl;
+
+    Time aida(3, 35);
+    Time tosca(2, 48);
+    Time temp;
+
+    cout << "Aida and Tosca:" << endl;
+    cout << aida << "; " << tosca << endl;
+    temp = aida + tosca;
+    temp = aida * 1.17;
+    cout << "Aida * 1.17: " << temp << endl;
+    cout << "10 * Tosca: " << 10 * tosca << endl;
+
+    return 0;
+}
