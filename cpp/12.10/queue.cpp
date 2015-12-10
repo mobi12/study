62c87aabc61978e77cc8d58ab65f917759a418bd
mobi12 <mc1a1@hotmail.com>
2015-12-10 21:28:47 +0800

    程序清单12.11--队列模拟

---
+++ cpp/12.10/queue.cpp
@@ -0,0 +1,78 @@
+#include <cstdlib>
+#include "queue.h"
+
+Queue::Queue(int qs):qsize(qs)
+{
+    front = rear = NULL;
+    items = 0;
+}
+
+Queue::~Queue()
+{
+    Node * temp;
+    while (front != NULL)
+    {
+        temp = front;
+        front = front->next;
+        delete temp;
+    }
+}
+
+bool Queue::isempty() const
+{
+    return items == 0;
+}
+
+bool Queue::isfull() const
+{
+    return items == qsize;
+}
+
+int Queue::queuecount() const
+{
+    return items;
+}
+
+bool Queue::enqueue(const Item & item)
+{
+    if (isfull())
+        return false;
+    Node * add = new Node;
+    if (add == NULL)
+        return false;
+
+    add->item = item;
+    add->next = NULL;
+    items++;
+
+    if (front == NULL)
+        front = add;
+    else
+        rear->next = add;
+    rear = add;
+
+    return true;
+}
+
+bool Queue::dequeue(Item & item)
+{
+    if (front == NULL)
+        return false;
+
+    item = front->item;
+    items--;
+    Node * temp = front;
+    front = front->next;
+    delete temp;
+
+    if (items == 0)
+        return NULL;
+
+    return true;
+}
+
+void Customer::set(long when)
+{
+    processtime = std::rand() % 3 + 1;
+    arrive = when;
+}
