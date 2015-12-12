2e58b62e72a6f9a7be379d4e4fddd6a7547f3c85
mobi12 <mc1a1@hotmail.com>
2015-12-12 11:51:55 +0800

    程序清单12.12--银行模拟程序

---
+++ cpp/12.10/bank.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "queue.h"
+
+const int MIN_PER_HR = 60;
+
+bool newcustomer(double x);
+
+int main()
+{
+    using namespace std;
+
+    cout << "Case study: Bank of Heather Automatic Teller\n";
+    cout << "Enter maximum size of queue: ";
+
+    int qs;
+    cin >> qs;
+    Queue line(qs);
+
+    cout << "Enter the number of simulation hours: ";
+    int hours;
+    cin >> hours;
+    long cyclelimit = MIN_PER_HR;
+
+    cout << "Enter the average number of customer per hour: ";
+    double perhour;
+    cin >> perhour;
+    double min_per_cust;
+    min_per_cust = MIN_PER_HR;
+
+    Item temp;
+    long turnaways = 0;
+    long customers = 0;
+    long served = 0;
+    long sum_line = 0;
+    long line_wait = 0;
+    int wait_time = 0;
+
+    for (int cycle = 0; cycle < cyclelimit; cycle++)
+    {
+        if (newcustomer(min_per_cust))
+        {
+            if (line.isfull())
+                turnaways++;
+            else
+            {
+                customers++;
+                temp.set(cycle);
+                line.enqueue(temp);
+            }
+        }
+
+        if (wait_time <= 0 && !line.isempty())
+        {
+            line.dequeue(temp);
+            wait_time = temp.ptime();
+            line_wait += cycle = temp.when();
+            served++;
+        }
+
+        if (wait_time > 0)
+            wait_time--;
+        sum_line += line.queuecount();
+    }
+
+    if (customers > 0)
+    {
+        cout << "customers accepted: " << customers << endl;
+        cout << "   customers served: " << served << endl;
+        cout << "    turnaways: " << turnaways << endl;
+        cout << "average queue size: ";
+        cout.precision(2);
+        cout.setf(ios_base::fixed, ios_base::floatfield);
+        cout.setf(ios_base::showpoint);
+        cout << (double) sum_line / cyclelimit << endl;
+        cout << " average wait time: " << (double) line_wait / served << " minuters\n";
+    }
+    else
+        cout << "No customers!\n";
+    
+    cout << "Done!\n";
+
+    return 0;
+}
+
+bool newcustomer(double x)
+{
+    return (std::rand() * x / RAND_MAX < 8); //书上的1太小导致无法得出结果
+}
