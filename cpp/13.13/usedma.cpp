cabacfd1dde9b834b7a6d7fe60faead9623d7e27
mobi12 <mc1a1@hotmail.com>
2015-12-23 14:22:13 +0800

    程序清单13.13--类继承中的友元和动态内存分配

---
+++ cpp/13.13/usedma.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "dma.h"
+
+int main()
+{
+	using std::cout;
+	using std::endl;
+	
+	baseDMA shirt("Portabelly", 8);
+	lacksDMA balloon("red", "Blimpo", 4);
+	hasDMA map("Mercator", "Buffalo Keys", 5);
+	
+	cout << shirt << endl;
+	cout << balloon << endl;
+	cout << map << endl;
+	
+	lacksDMA balloon2(balloon);
+	hasDMA map2;
+	map2 = map;
+	
+	cout << balloon2 << endl;
+	cout << map2 << endl;
+	
+	return 0;
+}
