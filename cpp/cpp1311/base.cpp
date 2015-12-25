c227197f1d812c4ee8e3ca4cf5869d449b1c52a1
mobi12 <mc1a1@hotmail.com>
2015-12-25 14:59:12 +0800

    有一个小错误

--- cpp/cpp1311/base.cpp
+++ cpp/cpp1311/base.cpp
@@ -9,7 +9,7 @@ Cd::Cd(const char *s1, const char *s2, int n, double x)
 {
     std::strncpy(performers, s1, 49);
     performers[49] = '\0';
-    std::strncpy(label, s2, 19);
+    std::strncpy(label, s3, 19);
     label[19] = '\0';
     selections = n;
     playtime = x;
@@ -53,7 +53,7 @@ Cd & Cd::operator =(const Cd & d)
     return *this;
 }
 
-Classic::Classic(const char *s1, const char *s2, const char *s3, int n, double x) : Cd(s1, s2, n, x)
+Classic::Classic(const char *s1, const char *s2, const char *s3, int n, double x) : Cd(s1, s3, n, x)
 {
     std::strncpy(name, s2, 49);
     name[49] = '\0';
