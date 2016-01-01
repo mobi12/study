94fcd8d40ce95957e59786e853d3fc1b9be1a8a8
mobi12 <mc1a1@hotmail.com>
2016-01-01 22:27:05 +0800

    程序清单14.1--student类

---
+++ cpp/14.1/student.h
@@ -0,0 +1,32 @@
+#ifndef STUDENT_H_
+#define STUDENT_H_
+
+#include <iostream>
+#include <string>
+#include <valarray>
+
+class Student
+{
+    private:
+        typedef std::valarry<double> ArrayDb;
+        std::string name;
+        ArrayDb scores;
+        std::ostream & arr_out(std::ostream & os) const;
+    public:
+        Student() : name("Null Student"), scores() {}
+        Student(const char std::string & s) : name(s), scores() {}
+        explicit Student(int n) : name("Nully"), scores(n) {}
+        Student(const std::string & s, int n) : name(s), scores(n) {}
+        Student(const std::string & s, const ArrayDb & a) : name(s), scores(a) {}
+        Student(const char *str, const double *pd, int n) : name(str), scores(pd, n) {}
+        ~Student() {}
+        double Average() const;
+        const std::string & Name() const;
+        double & operator [](int i);
+        double operator [](int i) const;
+        friend std::istream & operator >>(std::istream & is, Student & stu);
+        friend std::istream & getline(std::istream & is, Student & stu);
+        friend std::ostream & operator <<(std::ostream & os, const Student & stu);
+};
+
+#endif
