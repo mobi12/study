65608d58078d24f58b8e901eb94b6bcea5912ffe
mobi12 <mc1a1@hotmail.com>
2016-01-10 17:39:48 +0800

    python in操作

---
+++ python/pin.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+database = [
+    ['albert', '1234'],
+    ['dilbert', '4242'],
+    ['smith', '7524'],
+    ['jones', '9843']
+]
+
+username = raw_input('User name: ')
+pin = raw_input('PIN code: ')
+
+if [username, pin] in database : print 'Access granted'
