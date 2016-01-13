3ef3d62f752d998afc82fb0004b904408e8d3e23
mobi12 <mc1a1@hotmail.com>
2016-01-13 22:01:01 +0800

    程序清单14.21--类模板参数模板

---
+++ cpp/14.21/tempparm.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "stackpt.h"
+
+template <template <class t> class Thing>
+class Crab
+{
+    private:
+        Thing<int> s1;
+        Thing<double> s2;
+    public:
+        Crab() {}
+        bool push(int a, double x) { return s1.push(a) && s2.push(x); }
+        bool pop(int & a, double & x) { return s1.pop(a) && s2.pop(x); }
+};
+
+int main()
+{
+    using std::cout;
+    using std::cin;
+    using std::endl;
+    
+    Crab<Stack> nebula;
+    int ni;
+    double nb;
+    
+    cout << "Enter int double pairs, such as4 3.5 (0 0 to be end) :\n";
+    while (cin >> ni >> nb && ni > 0 && nb > 0)
+    {
+        if (!nebula.push(ni, nb))
+            break;
+    }
+    
+    while (nebula.pop(ni, nb))
+        cout << ni << ", " << nb << endl;
+    cout << "Done.\n";
+    
+    return 0;
+}
