63c238e0bab346caeec5d31d69d32a407f80c71f
mobi12 <mc1a1@hotmail.com>
2016-01-16 15:33:27 +0800

    我都不知道做了什么改进

---
+++ cpp/manyfrnd.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+template <typename T>
+class ManyFriend
+{
+    private:
+        T item;
+    public:
+        ManyFriend(const T & i) : item(i) {}
+        template <typename C, typename D> friend void show2(C & c, D & d);
+};
+
+template <typename C, typename D> void show2(C & c, D & d)
+{
+    cout << c.item << ", " << d.item << endl;
+}
+
+int main()
+{
+    ManyFriend<int> hfi1(10);
+    ManyFriend<int> hfi2(20);
+    ManyFriend<double> hfdb(10.5);
+    
+    cout << "hfi1, hfi2: ";
+    show2(hfi1, hfi2);
+    cout << "hfdb, hfi2: ";
+    show2(hfdb, hfi2);
+    
+    return 0;
+}
