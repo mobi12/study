1a6a91acab0801abe87bc5055fc485980d910b97
mobi12 <mc1a1@hotmail.com>
2016-01-24 22:00:20 +0800

    程序清单16.4--string类的内存分配

---
+++ cpp/str2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string empty;
+    string small = "bit";
+    string larger = "Elephants are a girl's best friend";
+    
+    cout << "Sizes:\n";
+    cout << "\tempty: " << empty.size() << endl;
+    cout << "\tsmall: " << small.size() << endl;
+    cout << "\tlarger: " << larger.size() << endl;
+    cout << "Capacities:\n";
+    cout << "\tempty: " << empty.capacity() << endl;
+    cout << "\tsmall: " << small.capacity() << endl;
+    cout << "\tlarger: " << larger.capacity() << endl;
+    empty.reserve(50);
+    cout << "Capacity after empty.reserve(50): "
+         << empty.capacity() << endl;
+    
+    return 0;
+}
