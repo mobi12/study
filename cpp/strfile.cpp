1e901d17ded4da3f455e228c3a93ec90300405e9
mobi12 <mc1a1@hotmail.com>
2016-01-24 10:50:12 +0800

    程序清单16.2--string类输入

---
+++ cpp/strfile.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    ifstream fin;
+    fin.open("tobuy.txt");
+
+    if (fin.is_open() == false)
+    {
+        cerr << "Can't open file. Bye.\n";
+        exit(EXIT_FAILURE);
+    }
+
+    string item;
+    int count = 0;
+
+    getline(fin, item, ',');
+    while (fin)
+    {
+        ++count;
+        cout << count << ": " << item << endl;
+        getline(fin, item, ',');
+    }
+
+    cout << "Done.\n";
+    fin.close();
+    return 0;
+}
