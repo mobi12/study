469771be78688e87dfd967232c837340e0aace68
mobi12 <mc1a1@hotmail.com>
2016-01-31 18:25:53 +0800

    程序清单16.10--stl序列容器list类

---
+++ cpp/list.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <list>
+#include <iterator>
+
+using namespace std;
+
+int main()
+{
+	list<int> one(5, 2);
+	int stuff[5] = { 1, 2, 3, 4, 5 };
+	list<int> two;
+	two.insert(two.begin(), stuff,)
+}
