006ee96e221c1b57daf97075ae290a6a19f6b3f6
mobi12 <mc1a1@hotmail.com>
2016-02-01 22:09:32 +0800

    程序清单16.12--multimap容器类

---
+++ cpp/multimap.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+typedef int KeyType;
+typedef pair<const KeyType, std::string> Pair;
+typedef multimap<KeyType, string> MapCode;
+
+int main()
+{
+    MapCode codes;
+
+    codes.insert(Pair(415, "San Francisco"));
+    codes.insert(Pair(510, "Oakland"));
+    codes.insert(Pair(718, "Brooklyn"));
+    codes.insert(Pair(718, "Staten Island"));
+    codes.insert(Pair(415, "San Rafael"));
+    codes.insert(Pair(510, "Berkeley"));
+
+    cout << "Number of cities with area code 415: "
+         << codes.count(415) << endl;
+    cout << "Number of cities with area code 718: "
+         << codes.count(718) << endl;
+    cout << "Number of cities with area code 510: "
+         << codes.count(510) << endl;
+    cout << "Area Code      City\n";
+    MapCode::iterator it;
+    for (it = codes.begin(); it != codes.end(); it++)
+        cout << "    " << (*it).first << "    "
+             << (*it).second << endl;
+    pair<MapCode::iterator, MapCode::iterator> range
+        = codes.equal_range(718);
+    cout << "Cities with area code 718: \n";
+    for (it = range.first; it != range.second; it++)
+        cout << (*it).second << endl;
+
+    return 0;
+}
