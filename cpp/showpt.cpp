9969567c540113cabc56f6d3d3be5517eefc2368
mobi12 <mc1a1@hotmail.com>
2016-02-15 13:34:51 +0800

    程序清单17.7--显示小数点

---
+++ cpp/showpt.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float price1 = 20.40;
+    float price2 = 1.9 + 8.0 / 9.0;
+
+    cout.setf(ios_base::showpoint);
+    cout << "\"Furry Friends\" is $" << price1 << "!\n";
+    cout << "\"Fiery Friends\" is $" << price2 << "!\n";
+
+    cout.precision(2);
+    cout << "\"Furry Friends\" is $" << price1 << "!\n";
+    cout << "\"Fiery Friends\" is $" << price2 << "!\n";
+
+    return 0;
+}
