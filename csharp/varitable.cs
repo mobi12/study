5ba80d8535b2cb3f8bbb16449369018a214b9448
mobi12 <mc1a1@hotmail.com>
2016-06-17 20:11:12 +0800

    csharp变量

---
+++ csharp/varitable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Varriables
+{
+	class Program
+	{
+		static int Main()
+		{
+			int x;
+			int y;
+			string myFirstName;
+			string test = "5";
+
+			x = 7;
+			y = x + 3;
+			myFirstName = "Stack";
+			var z = myFirstName + y.ToString();
+			x = int.Parse (test);
+
+			Console.WriteLine (z);
+			Console.WriteLine (x);
+			Console.ReadLine ();
+
+			return 0;
+		}
+	}
+}
