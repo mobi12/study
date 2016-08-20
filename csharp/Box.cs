b37eb8a387875cb8fa19e71122fbc23a0e31432f
unknown <>
2016-08-20 13:13:33 +0800

    Object类

---
+++ csharp/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Test
+{
+    class test
+    {
+        public static int Main()
+        {
+            int target = 10;
+            Object box = target;
+
+            Console.WriteLine(box);
+
+            int val = (int)box;
+            Console.WriteLine(val);
+            Console.ReadKey();
+
+            return 0;
+        }
+    }
+}
