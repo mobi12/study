b9fb09209b37d6f987d39fc879b6ef4e06ef09e0
mobi12 <mc1a1@hotmail.com>
2016-09-15 21:28:08 +0800

    做了一些小更改

--- python/bool.py
+++ python/bool.py
@@ -1,5 +1,7 @@
 age = input("输入你的年龄")
-if age >= "18":
-    print('ok')
-else:
-    print('no')
+if age >= "18" and age <= "20":
+    print('no man')
+elif age > "20" or age <= "40":
+    print('a man')
+elif age > "41" or age < "70":  
+    print("so old")
