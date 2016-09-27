54faadd9e5d4d4aba881f4e3a6094278a6134f00
mobi12 <mc1a1@hotmail.com>
2016-09-27 14:40:49 +0800

    C#枚举

---
+++ csharp/enum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Example
+{
+    public enum TimeOfDay { Morning = 0, Afternoon = 1, Evening = 2 }
+
+    class EnumExample
+    {
+        public static int Main(string[] args)
+        {
+            WriteGreeting(TimeOfDay.Morning);
+
+            return 0;
+        }
+
+        static void WriteGreeting(TimeOfDay timeOfDay)
+        {
+            switch (timeOfDay)
+            {
+                case TimeOfDay.Morning:
+                    Console.WriteLine("Good Morning");
+                    Console.ReadKey();  break;
+                case TimeOfDay.Afternoon:
+                    Console.WriteLine("Good Afternoon");
+                    Console.ReadKey();  break;
+                case TimeOfDay.Evening:
+                    Console.WriteLine("Good Evening");
+                    Console.ReadKey();  break;
+            }
+        }
+    }
+}
